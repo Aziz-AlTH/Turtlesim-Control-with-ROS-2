ac3839f162cec75ade1dc9f237152ca742c6918d
Abdulaziz Al-Thabeti <66jqqjc@gmail.com>
2025-08-04 10:48:07 +0300

    Add files via upload

---
+++ move_turtle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import Twist
+
+class TurtleController(Node):
+    def __init__(self):
+        super().__init__('turtle_controller')
+        self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
+        self.timer = self.create_timer(0.5, self.move_turtle)
+
+    def move_turtle(self):
+        msg = Twist()
+        msg.linear.x = 2.0     # تحريك للأمام
+        msg.angular.z = 1.0    # دوران بسيط
+        self.publisher_.publish(msg)
+        self.get_logger().info('Moving turtle forward with slight turn')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TurtleController()
+    rclpy.spin(node)
+    node.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
